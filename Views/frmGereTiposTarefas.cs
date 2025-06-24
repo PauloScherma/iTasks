09e751300e8f3dee8ab63ec890da4a1a0e05c752
MiguelCosta06 <2241612@my.ipleiria.pt>
2025-06-24 21:24:51 +0100

    Correção de erros, mensagens de aviso e crud do TipoTarefas

--- Views/frmGereTiposTarefas.cs
+++ Views/frmGereTiposTarefas.cs
@@ -24,8 +24,48 @@ namespace iTasks
         {
             string descricao = txtDesc.Text;
 
+            if (descricao == string.Empty)
+            {
+                MessageBox.Show("Por favor, dê uma descrição", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            using (var db = new ITaskContext())
+            {
+                // Verifica se já existe um Tipo de Tarefa com essa descrição
+                var tipoTarefaExistente = db.TiposTarefa.FirstOrDefault(t => t.Nome == descricao);
+                if (tipoTarefaExistente != null)
+                {
+                    MessageBox.Show("Já existe um Tipo de Tarefa com essa descrição.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+
             frmGereTiposTarefasController.gravarDados(descricao);
+            lstLista.DataSource = null;
             lstLista.DataSource = frmGereTiposTarefasController.mostrarTiposTarefas();
+        }
+
+        private void btExcluirTipoTarefa_Click(object sender, EventArgs e)
+        {
+            int indexSelecionado = lstLista.SelectedIndex;
+            TipoTarefa TipoTarefaSelecionado = (TipoTarefa)lstLista.SelectedItem;
+            int IdTipotarefa = TipoTarefaSelecionado.Id;
+            frmGereTiposTarefasController.excluirTipoTarefa(IdTipotarefa);
+
+            //atualiza as listBoxs
+            lstLista.DataSource = null;
+            lstLista.DataSource = frmGereTiposTarefasController.mostrarTiposTarefas();
+        }
+
+        private void lstLista_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstLista.SelectedIndex != -1)
+            {
+                TipoTarefa tipoSelecionado = (TipoTarefa)lstLista.SelectedItem;
+                txtDesc.Text = tipoSelecionado.Nome;
+                txtId.Text = tipoSelecionado.Id.ToString();
+            }
         }
     }
 }
